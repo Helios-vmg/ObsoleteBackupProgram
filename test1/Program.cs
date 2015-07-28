3c01e4937bdce70ef0a84917f34f2baa2ef0274d
Víctor M. González <victor.gonzalez@nektra.com>
2015-07-28 17:52:39 -0300

    Basic command processor implementation.

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -76,8 +77,16 @@ namespace test1
         public HashSet<string> IgnoredPaths =
             new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
 
-        public HashSet<string> IgnoredNames = new HashSet<string>(DefaultIgnoredExtensions,
-            StringComparer.InvariantCultureIgnoreCase);
+        [Flags]
+        public enum NameIgnoreType
+        {
+            None = 0,
+            File = 1,
+            Directory = 2,
+            All = 3,
+        }
+
+        public Dictionary<string, NameIgnoreType> IgnoredNames = new Dictionary<string, NameIgnoreType>(StringComparer.InvariantCultureIgnoreCase);
 
         public Backupper(string targetPath) : base(targetPath)
         {
@@ -120,6 +129,110 @@ namespace test1
     }
 #endif
 
+    class ProgramState
+    {
+        private Backupper _backupper;
+
+        public void ProcessLine(string[] line)
+        {
+            switch (line[0].ToLower())
+            {
+                case "open":
+                    ProcessOpen(line);
+                    break;
+                case "add":
+                    ProcessAdd(line);
+                    break;
+                case "exclude":
+                    ProcessExclude(line);
+                    break;
+                case "backup":
+                    ProcessBackup();
+                    break;
+                case "restore":
+                    ProcessRestore();
+                    break;
+            }
+        }
+
+        public void ProcessOpen(string[] line)
+        {
+            _backupper = new Backupper(line[1]);
+        }
+
+        private void ProcessAdd(string[] line)
+        {
+            _backupper.Sources.Add(line[1]);
+        }
+
+        public void ProcessExclude(string[] line)
+        {
+            switch (line[1].ToLower())
+            {
+                case "extension":
+                    ProcessExcludeExtension(line);
+                    break;
+                case "path":
+                    ProcessExcludePath(line);
+                    break;
+                case "name":
+                    ProcessExcludePath(line);
+                    break;
+            }
+        }
+
+        public void ProcessExcludeExtension(string[] line)
+        {
+            _backupper.IgnoredExtensions.Add(line[2]);
+        }
+
+        public void ProcessExcludePath(string[] line)
+        {
+            _backupper.IgnoredPaths.Add(line[2]);
+        }
+
+        public void ProcessExcludeName(string[] line)
+        {
+            switch (line[2].ToLower())
+            {
+                case "files":
+                    ProcessExcludeNameFiles(line);
+                    break;
+                case "dirs":
+                    ProcessExcludeNameDirs(line);
+                    break;
+                case "all":
+                    ProcessExcludeNameAll(line);
+                    break;
+            }
+        }
+
+        public void ProcessExcludeNameFiles(string[] line)
+        {
+            _backupper.IgnoredNames[line[3]] = Backupper.NameIgnoreType.File;
+        }
+
+        public void ProcessExcludeNameDirs(string[] line)
+        {
+            _backupper.IgnoredNames[line[3]] = Backupper.NameIgnoreType.Directory;
+        }
+
+        public void ProcessExcludeNameAll(string[] line)
+        {
+            _backupper.IgnoredNames[line[3]] = Backupper.NameIgnoreType.All;
+        }
+
+        public void ProcessBackup()
+        {
+            _backupper.PerformBackup();
+        }
+
+        public void ProcessRestore()
+        {
+            _backupper.RestoreBackup();
+        }
+    }
+
     class Program
     {
         private static BackupMode Filter(FileSystemObject fso)
@@ -140,8 +253,62 @@ namespace test1
             }
         }
 
+        static Queue<char> ToQueue(string s)
+        {
+            var ret = new Queue<char>();
+            bool spaces = true;
+            foreach (var c in s)
+            {
+                if (!spaces || c != ' ' && c != '\t')
+                {
+                    spaces = false;
+                    ret.Enqueue(char.ToLower(c, CultureInfo.CurrentCulture));
+                }
+            }
+            return ret;
+        }
+
+        [DllImport("shell32.dll", SetLastError = true)]
+        static extern IntPtr CommandLineToArgvW(
+            [MarshalAs(UnmanagedType.LPWStr)] string lpCmdLine, out int pNumArgs);
+
+        public static string[] CommandLineToArgs(string commandLine)
+        {
+            int argc;
+            var argv = CommandLineToArgvW(commandLine, out argc);
+            if (argv == IntPtr.Zero)
+                throw new System.ComponentModel.Win32Exception();
+            try
+            {
+                var args = new string[argc];
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var p = Marshal.ReadIntPtr(argv, i * IntPtr.Size);
+                    args[i] = Marshal.PtrToStringUni(p);
+                }
+
+                return args;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(argv);
+            }
+        }
+
         static void Main(string[] args)
         {
+            var state = new ProgramState();
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                var array = CommandLineToArgs(line);
+                if (array.Length == 0)
+                    continue;
+                if (array[0].Equals("quit", StringComparison.CurrentCultureIgnoreCase))
+                    break;
+                state.ProcessLine(array);
+            }
         }
     }
 }
