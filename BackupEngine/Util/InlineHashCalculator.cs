0a4c21d7b6a91ce4ea3786abc8f1aba3b68951b6
Víctor M. González <victor.gonzalez@nektra.com>
2015-08-04 14:08:25 -0300

    Implemented InlineHashCalculator.

---
+++ BackupEngine/Util/InlineHashCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BackupEngine.Util
+{
+    public class InlineHashCalculator
+    {
+        private Stream _inputStream,
+                       _outputStream;
+        private HashAlgorithm _hashAlgorithm;
+        private const int DefaultSize = 1 << 12;
+        private byte[] _inputBuffer,
+                       _hashBuffer,
+                       _outputBuffer;
+        private AutoResetEvent _hashReadyEvent,
+                               _writerReadyEvent,
+                               _done;
+
+        public InlineHashCalculator(Stream input, HashAlgorithm hash, Stream output)
+        {
+            _inputStream = input;
+            _hashAlgorithm = hash;
+            _outputStream = output;
+        }
+
+        public void DoCopy()
+        {
+            _hashReadyEvent = new AutoResetEvent(true);
+            _writerReadyEvent = new AutoResetEvent(true);
+            _done = new AutoResetEvent(false);
+            ReadMore();
+            _done.WaitOne();
+        }
+
+        private void ReadMore()
+        {
+            _inputBuffer = new byte[DefaultSize];
+            var task = _inputStream.ReadAsync(_inputBuffer, 0, _inputBuffer.Length);
+            task.ContinueWith(FinishedRead);
+        }
+
+        private void FinishedRead(Task<int> task)
+        {
+            _hashReadyEvent.WaitOne();
+            _writerReadyEvent.WaitOne();
+            if (task.Result == 0)
+            {
+                _done.Set();
+                return;
+            }
+            _hashBuffer = _inputBuffer;
+            ReadMore();
+            _outputBuffer = _hashBuffer;
+            WriteMore(task.Result);
+            _hashAlgorithm.TransformBlock(_hashBuffer, 0, task.Result, null, 0);
+            _hashReadyEvent.Set();
+        }
+
+        private void WriteMore(int count)
+        {
+            var task = _outputStream.WriteAsync(_outputBuffer, 0, count);
+            task.ContinueWith(FinishedWrite);
+        }
+
+        private void FinishedWrite(Task task)
+        {
+            _writerReadyEvent.Set();
+        }
+    }
+}
