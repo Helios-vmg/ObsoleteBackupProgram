97571d8c65a44d58829ef4e941dbf77d9096a613
Víctor M. González <victor.gonzalez@nektra.com>
2015-08-04 14:35:56 -0300

    Hash calculator filters.

--- BackupEngine/Util/InlineHashCalculator.cs
+++ BackupEngine/Util/InlineHashCalculator.cs
@@ -73,4 +73,132 @@ namespace BackupEngine.Util
             _writerReadyEvent.Set();
         }
     }
+
+    public abstract class HashCalculatorFilter : Stream
+    {
+        protected Stream Stream;
+        protected HashAlgorithm Hash;
+
+        protected HashCalculatorFilter(Stream stream, HashAlgorithm hash)
+        {
+            Stream = stream;
+            Hash = hash;
+        }
+    }
+
+    public class HashCalculatorInputFilter : HashCalculatorFilter
+    {
+
+        public HashCalculatorInputFilter(Stream stream, HashAlgorithm hash) : base(stream, hash) {}
+
+        public override void Flush()
+        {
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var ret = Stream.Read(buffer, offset, count);
+            Hash.TransformBlock(buffer, offset, ret, null, 0);
+            return ret;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override bool CanRead
+        {
+            get { return true; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return false; }
+        }
+
+        public override long Length
+        {
+            get { return Stream.Length; }
+        }
+
+        public override long Position
+        {
+            get { throw new InvalidOperationException(); }
+            set { throw new InvalidOperationException(); }
+        }
+    }
+
+    public class HashCalulatorOutputFilter : HashCalculatorFilter
+    {
+        public HashCalulatorOutputFilter(Stream stream, HashAlgorithm hash) : base(stream, hash) { }
+
+        public override void Flush()
+        {
+            Stream.Flush();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new InvalidOperationException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            var task = Stream.WriteAsync(buffer, offset, count);
+            Hash.TransformBlock(buffer, offset, count, null, 0);
+            task.Wait();
+        }
+
+        public override bool CanRead
+        {
+            get { return false; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return false; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return true; }
+        }
+
+        public override long Length
+        {
+            get { throw new InvalidOperationException(); }
+        }
+
+        public override long Position
+        {
+            get { throw new InvalidOperationException(); }
+            set { throw new InvalidOperationException(); }
+        }
+    }
 }
