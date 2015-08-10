c830c73fe652b0e6de645f54fa16a039c480bf16
Víctor M. González <helios.vmg@gmail.com>
2015-08-10 05:30:59 -0300

    Implemented native part of reparse point creation.

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace test1
+﻿using System.Runtime.InteropServices;
+
+namespace test1
 {
     class Program
     {
+        [DllImport("BackupEngineNativePart64.dll", CharSet = CharSet.Unicode,
+            CallingConvention = CallingConvention.Cdecl)]
+        private static extern void test_func(string path);
+
         static void Main(string[] args)
         {
+            test_func(null);
+            return;
             var processor = new LineProcessor(args);
             processor.Process();
         }
